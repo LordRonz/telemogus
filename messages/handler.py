a681a3a086198ccfed5229225a0bb0685b8179a9
LordRonz <aarontanhar2000@gmail.com>
2022-02-16 21:48:10 +0700

    feat: add unknown command handler :rocket:

--- messages/handler.py
+++ messages/handler.py
@@ -1,5 +1,7 @@
 from telegram.ext import Dispatcher
 from messages.sus import sus_handler
+from messages.unknown import unknown_handler
 
 def message_handler(dispatcher: Dispatcher):
     dispatcher.add_handler(sus_handler)
+    dispatcher.add_handler(unknown_handler)
---
+++ messages/unknown.py
@@ -0,0 +1,9 @@
+from telegram import Update
+from telegram.ext import CallbackContext, MessageHandler, Filters
+
+def unknown(update: Update, context: CallbackContext):
+    if update.effective_chat is None:
+        return
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
