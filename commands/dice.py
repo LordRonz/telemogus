c85c8e268d33ff00d5b646501419681e637416dc
LordRonz <aarontanhar2000@gmail.com>
2022-02-22 15:27:04 +0700

    feat: add dice command :rocket:

---
+++ commands/dice.py
@@ -0,0 +1,11 @@
+from telegram import Update, Dice
+from telegram.ext import CallbackContext, CommandHandler
+from secrets import choice
+
+def dice(update: Update, context: CallbackContext):
+    if update.effective_chat is None:
+        return
+
+    context.bot.send_dice(chat_id=update.effective_chat.id, emoji=choice(Dice.ALL_EMOJI))
+
+dice_handler = CommandHandler('dice', dice)
--- commands/handler.py
+++ commands/handler.py
@@ -1,9 +1,11 @@
 from telegram.ext import Dispatcher
 from commands.start import start_handler
 from commands.chat_id import chatid_handler
+from commands.dice import dice_handler
 from commands.caps import inline_caps_handler
 
 def command_handler(dispatcher: Dispatcher):
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(chatid_handler)
+    dispatcher.add_handler(dice_handler)
     dispatcher.add_handler(inline_caps_handler)
