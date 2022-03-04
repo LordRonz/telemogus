382bc39fb12e3281aaf8905c995fd6ffd1daae7e
LordRonz <aarontanhar2000@gmail.com>
2022-03-04 11:15:55 +0700

    feat: use webhook for heroku :rocket:

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
     logger.info("Hello World!")
     updater.start_webhook(
         listen="0.0.0.0",
-        port=443,
+        port=8443,
         url_path=TOKEN,
         webhook_url=f"https://telemogus.herokuapp.com/{TOKEN}",
     )
